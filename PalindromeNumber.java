5cc60420a0e0a0fbfefdf9750b27b0d1707ced1c
saravana2156 <saravanakumar.mea@gmail.com>
2024-11-05 11:40:17 +0530

    Add files via upload

---
+++ PalindromeNumber.java
@@ -0,0 +1,24 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int reverse = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+
+        if (originalNumber == reverse) {
+            System.out.println(originalNumber + " is a palindrome.");
+        } else {
+            System.out.println(originalNumber + " is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
